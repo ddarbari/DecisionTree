0e09674b2a4e39f06d703ca8180cc444c24ae5fc
Divya Darbari <ddarbari@microsoft.com>
2017-04-16 15:15:46 -0700

    DecisionTree Test

--- ClickStreamMiningData.py
+++ ClickStreamMiningData.py
@@ -6,12 +6,12 @@ import collections
 # Define the node class
 class Node:
     def __init__(self):
-        self.attributeIndex = -1
-        self.children = []
+        self.attributeId = -1
+        self.children = {}
         self.label = "Internal Node"
-
-    def add_node(self, node):       
-        self.children.append(node);
+        
+    def addNode(self, node, attributeValue):       
+        self.children[attributeValue] = node;
 
 def printTree(node):
     print (node.label)
@@ -19,22 +19,22 @@ def printTree(node):
     print('--------------')
     
 # Learner's Algorithm
-def learner(attributelist, data):
+def Learner(attributelist, data):
     root = Node()
-    should_stop = stop_criteria(attributelist, data, root)
+    should_stop = stopCriteria(attributelist, data, root)
     if should_stop != True:
-        attributeId = choose_best_attribute(attributelist, data)
-        root.attributeId = attributeId
-        root.label = attributelist[attributeId][0]
-        for attribute_value in attributelist[attributeId][1]:
-            splitData = [row for row in data if row[attributeId] == attribute_value]
-            newattributelist = attributelist[:attributeId] + attributelist[(attributeId + 1):]
-            childNode = learner(newattributelist, splitData)
-            root.add_node(childNode)
+        attribute = chooseBestAttribute(attributelist, data)
+        root.attributeId = attributeNameList.index(attribute[0])
+        root.label = attribute[0]
+        for attributeValue in attribute[1]:
+            splitData = [row for row in data if row[root.attributeId] == attributeValue]
+            newattributelist = [i for i in attributelist if i[0] != root.label]
+            childNode = Learner(newattributelist, splitData)
+            root.addNode(childNode, attributeValue)
     return root
 
 # Stop_Criteria
-def stop_criteria(attributelist, data, root):
+def stopCriteria(attributelist, data, root):
     if(len(attributelist) == 0 or len(data)== 0):
         root.label = "Data or AttributeList is empty"
         return True
@@ -48,44 +48,90 @@ def stop_criteria(attributelist, data, root):
    
 
 #Calculates the entropy of the given data set for the target attribute
-def entropy(attribute, data):
-    result_True =  {}
-    result_False = {}
-    data_entropy = 0.0
-    i = 0
-    for entry in attributes:
-        if (targetAttr == entry):
-            break
-        ++i
-    
-    for entry in data:
-        if(entry[i] == True):
-            if (result_True.has_key(entry[i])):
-                result_True[entry[i]] += 1.0
+def entropy(attributeName, data):
+    entropy = 0
+    attributeIndex = attributeNameList.index(attributeName)
+    truedictList = {}
+    falsedictList = {}
+
+    for row in data:
+        if(row[len(attributelist)] == 'False'):
+            if not row[attributeIndex] in falsedictList:
+                falsedictList[row[attributeIndex]] = 1
             else:
-                result_True[entry[i]]  = 1.0
+                falsedictList[row[attributeIndex]] += 1
         else:
-            if (result_False.has_key(entry[i])):
-                result_False[entry[i]] += 1.0
+            if not row[attributeIndex] in truedictList:
+                truedictList[row[attributeIndex]] = 1
             else:
-                result_False[entry[i]]  = 1.0
-    return 0.3
+                truedictList[row[attributeIndex]] += 1
+
+    for value in attributelist[attributeIndex][1]:
+        if value in truedictList:
+            trueCount = truedictList[value]
+        else:
+            trueCount = 0
+        if value in falsedictList:
+            falseCount = falsedictList[value]
+        else:
+            falseCount = 0
+        totalOccurence = trueCount + falseCount
+
+        if totalOccurence==0:
+            continue
+        trueRatio= trueCount/totalOccurence
+        falseRatio = falseCount/totalOccurence
+        if trueRatio != 0:
+            entropy += -trueRatio*math.log(trueRatio, 2)
+        if falseRatio != 0:
+            entropy += -falseRatio*math.log(falseRatio, 2)
+    
+    return entropy
 
 #Calculates the best attribute to be chosen
-def choose_best_attribute(attributelist, data):
-    maxEntropy = 0
+def chooseBestAttribute(subAttributeList, data):
+    minEntropy = 100000
     bestAttribute = -1
-    for attributeId in attributelist:
-        if (entropy > maxEntropy):
-            maxEntropy = entropy 
-            bestAttribute = attributeId
-    return 2
-
+    for attribute in subAttributeList:
+        e = entropy(attribute[0], data)
+        if (e < minEntropy):
+            minEntropy = e 
+            bestAttribute = attribute
+    
+    return bestAttribute
+    
+def DecisionTreeTest():
+    datatest = arff.load(open('testingD.arff'))
+    dataTestArr = np.array(datatest['data'])
+    attributeTest = datatest['attributes']
+    node = root
+    
+    row  = 24212
+    while(node and node.attributeId != -1):
+        print(node.label)
+        print(node.attributeId)
+        attributeValue = dataTestArr[row][node.attributeId]
+        
+        print(attributeValue)
+        if (attributeValue in node.children):
+            node = node.children[attributeValue]
+        else:
+            break
+    
+    print(node.label) 
+    print(dataTestArr[row])
+    # TODO: check for equality of final response
 
 # Load the training data
-dataset = arff.load(open('Test.arff'))
+dataset = arff.load(open('training_subsetD.arff'))
 data = np.array(dataset['data'])
 attributelist = dataset['attributes']
+attributelist.pop()
 
-root = learner(attributelist, data)
-printTree(root)
+attributeNameList = [row[0] for row in attributelist]
+
+#root = Learner(attributelist, data)
+#print(data[0][root.attributeId])
+#print(attributelist[207][1].index(data[0][root.attributeId]))
+#DecisionTreeTest()
+#printTree(root)
